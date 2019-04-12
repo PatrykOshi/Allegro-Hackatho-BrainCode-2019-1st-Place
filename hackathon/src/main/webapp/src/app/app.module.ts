710e9ef3a10a4dbbb57afa68f6d0612ee2c57f56
Michał Lewandowski <kontakt@michalewandowski.com>
2019-04-13 00:59:01 +0200

    subscription component

--- hackathon/src/main/webapp/src/app/app.module.ts
+++ hackathon/src/main/webapp/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SubscriptionButtonComponent } from './subscription-button/subscription-button.component';
+import { SubscriptionComponent } from './subscription/subscription.component';
+import {BsDropdownModule, ModalModule, TooltipModule} from "ngx-bootstrap";
 
 @NgModule({
   declarations: [
     AppComponent,
-    SubscriptionButtonComponent
+    SubscriptionButtonComponent,
+    SubscriptionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
