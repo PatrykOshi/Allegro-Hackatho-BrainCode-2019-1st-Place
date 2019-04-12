f39f153052d83c5f686280df1f638dfb90fd7c02
Kulisiewicz <s13805@pjwstk.edu.pl>
2019-04-13 00:47:33 +0200

    Entities added

--- hackathon/src/main/java/sierpce/hackathon/controller/MainController.java
+++ hackathon/src/main/java/sierpce/hackathon/controller/MainController.java
@@ -2,9 +2,7 @@ package sierpce.hackathon.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
--- hackathon/src/main/java/sierpce/hackathon/model/entity/Vendor.java
+++ hackathon/src/main/java/sierpce/hackathon/model/entity/Vendor.java
@@ -1,4 +1,48 @@
 package sierpce.hackathon.model.entity;
 
-public class Vendor {
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "Vendor")
+public class Vendor{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String vendorName;
+
+    @OneToMany
+    private List<Product> productList;
+
+    public Vendor() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getVendorName() {
+        return vendorName;
+    }
+
+    public void setVendorName(String vendorName) {
+        this.vendorName = vendorName;
+    }
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    public void setProductList(List<Product> productList) {
+        this.productList = productList;
+    }
 }
+
