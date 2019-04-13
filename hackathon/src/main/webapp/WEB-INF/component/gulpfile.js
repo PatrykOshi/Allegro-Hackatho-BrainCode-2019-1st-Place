f528a65175478d1abaf10526ae8760f3c8e223dd
Michał Lewandowski <kontakt@michalewandowski.com>
2019-04-13 04:54:59 +0200

    subscription-component

---
+++ hackathon/src/main/webapp/WEB-INF/component/gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var bs = require('browser-sync').create();
+
+gulp.task('browser-sync', function() {
+    bs.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+gulp.task('sass', function () {
+    return gulp.src('scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('css'))
+        .pipe(bs.reload({stream: true}));
+});
+
+gulp.task('watch', function () {
+    gulp.watch("scss/*.scss", ['sass']);
+    gulp.watch("*.html").on('change', bs.reload);
+});
+
+gulp.task('default', gulp.parallel('browser-sync'));
