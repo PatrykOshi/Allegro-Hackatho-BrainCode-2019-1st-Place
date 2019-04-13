9c23ac7ed0709c5e3a7dc5beb90eefe4a2da798e
Kulisiewicz <s13805@pjwstk.edu.pl>
2019-04-13 03:35:54 +0200

    MainController bugfixed

--- hackathon/src/main/java/sierpce/hackathon/controller/MainController.java
+++ hackathon/src/main/java/sierpce/hackathon/controller/MainController.java
@@ -17,7 +17,7 @@ import sierpce.hackathon.service.VendorService;
 
 import java.util.List;
 
-@Controller
+@Controller("/allegro")
 @RequestMapping(name = "/allegro")
 public class MainController {
 
@@ -30,40 +30,39 @@ public class MainController {
     @Autowired
     private ProductService productService;
 
-    @GetMapping(name = "/produkty")
+    @RequestMapping
     //TODO api + attributes
     public ModelAndView getMainPage(){
         ModelAndView mav = new ModelAndView("index");
         return mav;
     }
-<<<<<<< HEAD
 
-    @GetMapping(name = "/produkt")
+    @RequestMapping(value = "/produkt")
     public ModelAndView getProductPage(){
         ModelAndView mav = new ModelAndView("productView");
         return mav;
     }
 
-    @GetMapping(name = "/subskrybcja/edytuj")
+    @RequestMapping(value = "/subskrybcja/edytuj")
     public ModelAndView getSubscriptionSummary(){
         ModelAndView mav = new ModelAndView("productOrder");
         return mav;
     }
 
-    @GetMapping(name = "/subskrypcja/podsumowanie")
-    public ModelAndView get(){
+    @RequestMapping(value = "/subskrypcja/podsumowanie")
+    public ModelAndView getSummary(){
         ModelAndView mav = new ModelAndView("productOrderSummary");
         return mav;
     }
 
-    @PostMapping("/subskrybcja/potwierdz")
+    @PostMapping(value = "/subskrybcja/potwierdz")
     public ModelAndView saveSubscription(@ModelAttribute("subscription")Subscription subscription){
         ModelAndView mav = new ModelAndView("redirect:/mojesubskrypcje");
         subscriptionService.addSubscription(subscription);
         return mav;
     }
 
-    @GetMapping(name = "/mojesubskrypcje")
+    @RequestMapping(value = "/mojesubskrypcje")
     public ModelAndView getUserSubscription(){
         ModelAndView mav = new ModelAndView("mySubscriptions");
         List<Subscription> subscriptionList = subscriptionService.getSubscriptions(userService.getUser());
