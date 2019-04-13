bc91514aa23a79a0dc41667d9abddf2606e89c6e
Kulisiewicz <s13805@pjwstk.edu.pl>
2019-04-13 03:27:25 +0200

    Processes updated

--- hackathon/src/main/java/sierpce/hackathon/controller/MainController.java
+++ hackathon/src/main/java/sierpce/hackathon/controller/MainController.java
@@ -1,14 +1,34 @@
 package sierpce.hackathon.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import sierpce.hackathon.model.entity.Product;
+import sierpce.hackathon.model.entity.Subscription;
+import sierpce.hackathon.model.entity.Vendor;
+import sierpce.hackathon.service.ProductService;
+import sierpce.hackathon.service.SubscriptionService;
+import sierpce.hackathon.service.UserService;
+import sierpce.hackathon.service.VendorService;
+
+import java.util.List;
 
 @Controller
 @RequestMapping(name = "/allegro")
 public class MainController {
+
+    @Autowired
+    private SubscriptionService subscriptionService;
+    @Autowired
+    private VendorService vendorService;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private ProductService productService;
 
     @GetMapping(name = "/produkty")
     //TODO api + attributes
@@ -36,14 +56,17 @@ public class MainController {
     }
 
     @PostMapping("/subskrybcja/potwierdz")
-    public ModelAndView saveSubscription(){
+    public ModelAndView saveSubscription(@ModelAttribute("subscription")Subscription subscription){
         ModelAndView mav = new ModelAndView("redirect:/mojesubskrypcje");
+        subscriptionService.addSubscription(subscription);
         return mav;
     }
 
     @GetMapping(name = "/mojesubskrypcje")
     public ModelAndView getUserSubscription(){
         ModelAndView mav = new ModelAndView("mySubscriptions");
+        List<Subscription> subscriptionList = subscriptionService.getSubscriptions(userService.getUser());
+        mav.addObject(subscriptionList);
         return mav;
     }
 
--- hackathon/src/main/java/sierpce/hackathon/service/VendorService.java
+++ hackathon/src/main/java/sierpce/hackathon/service/VendorService.java
@@ -2,6 +2,7 @@ package sierpce.hackathon.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import sierpce.hackathon.model.entity.Vendor;
 import sierpce.hackathon.model.repository.VendorRepo;
 
 @Service
@@ -10,5 +11,8 @@ public class VendorService {
     @Autowired
     private VendorRepo vendorRepo;
 
+    public Vendor getVendor(){
+        return vendorRepo.findAll().get(0);
+    }
 
 }
