918de015a85e35a5b01a70a238a748389840dbed
PatrykOshi <39466882+PatrykOshi@users.noreply.github.com>
2019-04-13 16:08:32 +0200

    Summary

--- hackathon/src/main/java/sierpce/hackathon/controller/MainController.java
+++ hackathon/src/main/java/sierpce/hackathon/controller/MainController.java
@@ -33,7 +33,7 @@ public class MainController {
     @RequestMapping
     //TODO api + attributes
     public ModelAndView getMainPage(){
-        ModelAndView mav = new ModelAndView("index");
+        ModelAndView mav = new ModelAndView("subscriptionDetails");
         return mav;
     }
 
